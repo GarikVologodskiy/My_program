b8666cbe01112f30f5fb19732764a1a88cc5c5d5
GarikVologodskiy <49877717+GarikVologodskiy@users.noreply.github.com>
2020-04-13 23:30:01 +0500

    My first Kotlin's programs

---
+++ src/My_program_3.kt
@@ -0,0 +1,15 @@
+fun getYearEra(numberOfYear: Int): String {
+    return when {
+        numberOfYear < 1970 -> "before"
+        numberOfYear == 1970 -> "equals"
+        numberOfYear in 1971..2000 -> "after (XX century)"
+        numberOfYear in 2001..2100 -> "after (XXI century)"
+        else -> "distant future"
+    }
+}
+
+fun main(args: Array<String>) {
+    val numberOfYearUser: String? = readLine()
+    val numberOfYear:Int = numberOfYearUser?.toInt() ?:return
+    getYearEra(numberOfYear).toString()
+}
